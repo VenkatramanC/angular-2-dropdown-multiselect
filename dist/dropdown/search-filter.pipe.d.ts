0d4ea547526dc5f043de0084d2bc7313bd2c0261
tmurphyHOM <teresa.murphy@myhominc.com>
2017-06-09 08:43:14 -0600

    removed dist

--- dist/dropdown/search-filter.pipe.d.ts
+++
@@ -1,5 +0,0 @@
-import { PipeTransform } from '@angular/core';
-import { IMultiSelectOption } from './types';
-export declare class MultiSelectSearchFilter implements PipeTransform {
-    transform(options: Array<IMultiSelectOption>, args: string): Array<IMultiSelectOption>;
-}
